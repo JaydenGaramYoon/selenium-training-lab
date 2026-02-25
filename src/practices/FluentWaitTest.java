a6cca1316774ff8e5378e83b22bf62750cee79aa
Garam Yoon <rkfka9536@gmail.com>
2026-02-25 15:03:35 -0500

    refactor: changed file name

--- src/practices/FluentWaitTest.java
+++ src/practices/FluentWaitTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class fluentWaitTest {
+public class FluentWaitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
