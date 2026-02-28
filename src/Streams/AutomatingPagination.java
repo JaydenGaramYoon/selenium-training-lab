c73edb99bbeed1763cbe5407afc2a03e16560f3c
Garam Yoon <rkfka9536@gmail.com>
2026-02-27 20:31:21 -0500

    add AutomatingPagination.java: learned how to automate pagination with Stream API

---
+++ src/Streams/AutomatingPagination.java
@@ -0,0 +1,53 @@
+package Streams;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+public class AutomatingPagination {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// test data
+		String targetItem = "Apple";
+		// open browser
+		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1000));
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+		// instantiate variables
+		List<String> items;
+		List<String> itemPrices;
+		// automating the pagination
+		do {
+			// get displayed item elements
+			List<WebElement> elementItems = driver.findElements(By.xpath("//tr/td[1]"));
+			// get displayed texts from item elements
+			items = elementItems.stream().filter(name -> name.getText().contains(targetItem))
+					.map(name -> name.getText()).collect(Collectors.toList());
+			System.out.println(items.contains(targetItem));
+			itemPrices = elementItems.stream().filter(name -> name.getText().contains(targetItem))
+					.map(item -> getPrice(item)).collect(Collectors.toList());
+			// check if the list has target item by using .contains()
+			if (!(items.size() > 0)) {
+				// move to next page
+				driver.findElement(By.cssSelector("a[aria-label='Next']")).click();
+				// restart the loop
+			}
+		} while (!items.contains(targetItem));
+		items.forEach(item -> System.out.println("collectionList:" + item));
+		itemPrices.forEach(price -> System.out.println("collectionList" + price));
+	}
+
+	public static String getPrice(WebElement item) {
+		String price = item.findElement(By.xpath("following-sibling::td[1]")).getText();
+		return price;
+	}
+}
