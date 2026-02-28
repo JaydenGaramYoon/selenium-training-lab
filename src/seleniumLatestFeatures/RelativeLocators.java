d6a5ee173d193f5b609175594fb155adcbe02720
Garam Yoon <rkfka9536@gmail.com>
2026-02-27 23:23:55 -0500

    add RelativeLocators.java: learned how to use relative locators

---
+++ src/seleniumLatestFeatures/RelativeLocators.java
@@ -0,0 +1,33 @@
+package seleniumLatestFeatures;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator.RelativeBy;
+
+import static org.openqa.selenium.support.locators.RelativeLocator.*;
+
+import org.openqa.selenium.By;
+
+public class RelativeLocators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		// .above()
+		WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
+		System.out.println(driver.findElement(with(By.tagName("label")).above(nameEditBox)).getText());
+		// .below()
+		// selenium cannot identify flex component
+		WebElement dOB = driver.findElement(By.cssSelector("[for='dateofBirth']"));
+		driver.findElement(with(By.tagName("input")).below(dOB)).click();
+		// .toLeftOf()
+		WebElement text = driver.findElement(By.xpath("//label[text()='Check me out if you Love IceCreams!']"));
+		driver.findElement(with(By.tagName("input")).toLeftOf(text)).click();
+		// .toRightOf()
+		WebElement rdBtn = driver.findElement(By.id("inlineRadio1"));
+		System.out.println(driver.findElement(with(By.tagName("label")).toRightOf(rdBtn)).getText());
+	}
+
+}
